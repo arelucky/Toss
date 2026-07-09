8ca030bb6ffe34b3ab636368d7429aa7adcce533
zhaoheng <799931557@qq.com>
2026-07-09 23:08:27 +0800

    chore: initialize Toss project

---
+++ Toss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toss
+//
+//  Created by henry on 2026/7/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toss/TossApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossApp.swift
+//  Toss
+//
+//  Created by henry on 2026/7/9.
+//
+
+import SwiftUI
+
+@main
+struct TossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
